9b0dd29b9d3f7dce51ba7d27162cd2036606addf
aaditya shewale <aadityashewale@aadityas-MacBook-Air.local>
2024-03-30 23:26:02 +0530

    Navbar and hero

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+const  App = ()=> {
+
+
+  return (
+   <main className="bg-black">
+    <Navbar/>
+    <Hero/>
+   
+   </main>
+   )
+}
+
+export default App
