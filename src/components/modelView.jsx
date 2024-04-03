ef14b01c9eb4b9ebbaacf760fbca3a3733b9ec0e
aaditya shewale <aadityashewale@aadityas-MacBook-Air.local>
2024-04-03 20:06:10 +0530

    model

---
+++ src/components/modelView.jsx
@@ -0,0 +1,16 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
+
+
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationState,size,item}) => {
+  return (
+    <View index={index} id={gsapType} className={`border-2 border-red-500 w-full h-full $ {index === 2} ? 'right-[-100%]:''`} >
+      <ambientLight intensity={0.3}/>
+      <PerspectiveCamera makeDefault position={[0, 0,4]} />
+
+    
+
+    </View>
+  )
+}
+
+export default ModelView
